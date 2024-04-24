c3eb40be8c24acc847b950dd379640ac9e48947e
Blackness69 <161008491+Blackness69@users.noreply.github.com>
2024-04-24 17:29:56 +0000

    .

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ client.on('ready', async () => {
     .setStartTimestamp(Date.now())
     .setAssetsLargeImage('https://cdn.discordapp.com/attachments/1209078672993165312/1232035529411792936/images_1_1.jpg?ex=6627fe00&is=6626ac80&hm=38980b252589ed501195b0578b6faa2ad3574db3eed3f29de29101e982bebbf9&')
     .setAssetsLargeText('Developer of CattyPal')
-    .setAssetsSmallImage('https://cdn.discordapp.com/attachments/1209078672993165312/1232035502157332510/IMG_20240422_193744.jpg?ex=6627fdf9&is=6626ac79&hm=b3c77e896ee05f3b9cf9708092d723f37ef9f68c74221a2348e69aa57b35a244&')
+    .setAssetsSmallImage('https://cdn.discordapp.com/attachments/1209078672993165312/1232744735051874366/IMG_20240423_210648-removebg-preview.png?ex=662a9280&is=66294100&hm=1f857223fbec425669b783c584a260d25efea38926251b8f59c107584ecdeb79&')
     .setAssetsSmallText('🥀!NotBlackness </>')
         .addButton('CattyPal Support Server', "https://discord.com/invite/286Faxc5nd")
     .addButton('Add CattyPal', "https://discord.com/oauth2/authorize?client_id=1229438321395109929&permissions=8&scope=bot+applications.commands")
